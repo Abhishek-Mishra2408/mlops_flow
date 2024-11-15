dff40703bd1ec36fd8a41039228f6b9123527c62
Abhishek-Mishra2408 <abhisonu683@gmail.com>
2024-11-15 16:07:54 +0530

    added setup file

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = "vehicle_littering_detection",
+    version = "0.1.0",
+    description = "A machine learning project for vehicle littering detection",
+    author = "Abhishek Mishra",
+    author_email = "abhisonu683@gmail.com",
+    license = "MIT",
+    packages= find_packages()
+)
